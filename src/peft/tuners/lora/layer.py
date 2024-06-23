bb98060372e9a429218a482f5e13518807723ac1
linkevin281 <linkevin281@gmail.com>
2024-06-23 17:51:14 -0400

    Remove bns

--- src/peft/tuners/lora/layer.py
+++ src/peft/tuners/lora/layer.py
@@ -98,6 +98,11 @@ class LoraLayer(BaseTunerLayer):
 
     def __init__(self, base_layer: nn.Module, **kwargs) -> None:
         self.base_layer = base_layer
+<<<<<<< Updated upstream
+=======
+        self.rank = kwargs.get("r")
+        self.codebook_size = kwargs.get("codebook_size")
+>>>>>>> Stashed changes
         self.r = {}
         self.lora_alpha = {}
         self.scaling = {}
@@ -123,8 +128,13 @@ class LoraLayer(BaseTunerLayer):
             self.hr_vqlora = nn.ModuleList()  # Lists to hold quantization and normalization layers for each level
             self.bns = nn.ModuleList()
             for i in range(self.n_level):     # Initialize quantization layers, and batch norm layers
+<<<<<<< Updated upstream
                 self.hr_vqlora.append(Quantize(out_features, N_EMBED, self.decay))
                 self.bns.append(nn.BatchNorm1d(out_features))
+=======
+                self.hr_vqlora_A.append(Quantize(self.rank*in_features, self.codebook_size, self.decay))
+                self.hr_vqlora_B.append(Quantize(out_features*self.rank, self.codebook_size, self.decay))
+>>>>>>> Stashed changes
         else:
             raise ValueError(f"HR-VQLoRA only supports nn.Linear layers, found a {type(base_layer)}")
         # elif isinstance(base_layer, nn.Conv2d):
